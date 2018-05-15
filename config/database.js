35b1ccac4969bc6488100e9c18efbbb2be5e76fb
Higo Ribeiro <HORBJN@gmail.com>
2018-05-15 09:27:25 -0300

    Sequelize e configurações iniciais definidas

---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+  username: 'rocketseat',
+  password: null,
+  database: 'docfy',
+  host: '127.0.0.1',
+  dialect: 'mysql',
+};
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
+const path = require('path');
+const flash = require('connect-flash');
+const session = require('express-session');
+const sessionConfig = require('./config/session');
+const routes = require('./app/routes');
+
+const app = express();
+
+app.use(express.static(path.resolve('app', 'public')));
+
+nunjucks.configure(path.resolve('app', 'views'), {
+  autoescape: true,
+  express: app,
+});
+
+app.set('view engine', 'njk');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session(sessionConfig));
+app.use(flash());
+app.use('/', routes);
+
+app.listen(3000);
